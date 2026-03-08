2f9c5c76a1163990832b326651bbd216054d5f30
School Bloom <app@bloom.school>
2026-03-08 16:19:12 +0200

    Move seedDatabase import to top of file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
+import { seedDatabase } from './utils/seedData.js';
 
 // Import routes
 import authRoutes from './routes/authRoutes.js';
@@ -44,10 +45,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-import { seedDatabase } from './utils/seedData.js';
-
-// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/employees', employeeRoutes);
